6da10cee32629baf386b304b56084f3cb6901a29
rodrigo-cavt <rodriporter@gmail.com>
2025-09-29 11:45:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .catch(error => console.error('Cache install failed:', error))
   );
 });
 
@@ -18,5 +19,17 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(error => console.error('Fetch failed:', error))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
